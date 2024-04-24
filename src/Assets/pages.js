a2302424a026752cdc80d4ee6f7f2106baa63b3c
Eric Martinez Acelas <emartinezacelas@gmail.com>
2024-04-24 04:02:26 -0500

    switch render

--- src/Assets/pages.js
+++ src/Assets/pages.js
@@ -1,5 +1,5 @@
 const pages = {
-  this: "https://arcontroller-web.vercel.app/", //"https://arcontroller-web.onrender.com/","http://localhost:3001/", //
+  this: "https://arcontroller-web.onrender.com/", //"http://localhost:3001/", "https://arcontroller-web.vercel.app/", //
   remoteAPI: "https://api-publica.onrender.com/", //"http://localhost:2025/", //
   // local: ,
   // localAPI: ,

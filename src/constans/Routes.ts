2df678f550c0558b56111d677019e73f3d56475f
Eric Martinez Acelas <proyectos.sieng@gmail.com>
2024-10-24 23:57:56 -0500

    paths

--- src/constans/Routes.ts
+++ src/constans/Routes.ts
@@ -1,8 +1,8 @@
 import { ROUTES as RoutesDTO } from "@/dto";
 
 export const Routes: RoutesDTO = {
-  thisApp: "http://localhost:3000/", //
-  remoteApi: "http://localhost:2025/", //
+  thisApp: "https://arcontroller-web.onrender.com", //
+  remoteApi: "https://api-publica.onrender.com/", //
   // local: ,"https://arcontroller-web.onrender.com/", // "https://arcontroller-web.vercel.app/", //
   // localAPI: ,"https://api-publica.onrender.com/", //,
   Dashboard: "/Dashboard",
